2f7c21477a8c4c1023627ccff1a25f99ce5bb1aa
Daniel Kuster <116156820+dku-wd@users.noreply.github.com>
2024-04-08 14:30:08 +0200

    prepare

--- src/main/java/piano/TimeTableApp.java
+++ src/main/java/piano/TimeTableApp.java
@@ -35,7 +35,6 @@ public class TimeTableApp {
 
                 // The solver runs only for 5 seconds on this small dataset.
                 // It's recommended to run for at least 5 minutes ("5m") otherwise.
-                //.withTerminationSpentLimit(Duration.ofHours(9));
                 .withTerminationSpentLimit(Duration.ofSeconds(3));
         config.setEnvironmentMode(EnvironmentMode.REPRODUCIBLE);
 
